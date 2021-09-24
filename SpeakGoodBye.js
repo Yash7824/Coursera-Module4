48f294d2bed40525c5088cb437acc58d6887a14d
Yash <yashr7824@gmail.com>
2021-09-24 19:39:03 +0530

    Initial commit

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(byeSpeaker.speakWord + " " + name);
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var helloSpeaker = {};
+  helloSpeaker.speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    console.log(helloSpeaker.speakWord + " " + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
+
